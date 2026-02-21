5dbbfe0cc22b7f6a99dba349fa801558bda35a68
Aman Kumar <mramankumar215@gmail.com>
2026-02-21 21:09:59 +0530

    manage users functionality for managers

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -16,13 +16,13 @@ class BlogPostForm(forms.ModelForm):
         fields = ('title', 'category', 'featured_image', 'short_description', 'blog_body', 'status', 'is_featured')
 
 
-# class AddUserForm(UserCreationForm):
-#     class Meta:
-#         model = User
-#         fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+class AddUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
 
 
-# class EditUserForm(forms.ModelForm):
-#     class Meta:
-#         model = User
-#         fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
